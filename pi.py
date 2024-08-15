1475fef813acd5e0a8606988ab2881559cb24184
genfuru011 <h.furugen.24.7.1533@4nonome.com>
2024-08-15 21:42:07 +0900

    修正

--- pi.py
+++ pi.py
@@ -5,13 +5,7 @@ text = """
 
 # TODO
 
-sentence = (
-    "HOW I WANT A DRINK, ALCOHOLIC OF COURSE, AFTER THE HEAVY CHAPTERS INVOLVING "
-    "QUANTUM MECHANICS. ALL OF THY GEOMETRY, HERR PLANCK, IS FAIRLY HARD."
-)
-sentence = sentence.replace(",", "").replace(".", "")
-
-word_lengths = [len(word) for word in sentence.split()]
+word_lengths = list(map(len, ["how", "i", "want", "a", "drink", "alcoholic", "of", "course", "after", "the", "heavy", "chapters", "involving", "quantum", "mechanics", "All", "of", "thy", "geometry", "Herr", "Planck", "is", "fairly", "hard"]))
 
 result = ''.join(map(str, word_lengths))
 
